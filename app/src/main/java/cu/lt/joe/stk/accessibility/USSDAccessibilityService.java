05a806bdb6be6578d49a38daa33bba558c109ad7
jr20xx <pprichard0322@gmail.com>
2026-07-25 14:29:23 -0400

    Removed unused import

--- app/src/main/java/cu/lt/joe/stk/accessibility/USSDAccessibilityService.java
+++ app/src/main/java/cu/lt/joe/stk/accessibility/USSDAccessibilityService.java
@@ -4,7 +4,6 @@ import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.view.accessibility.AccessibilityEvent;
 import androidx.preference.PreferenceManager;
