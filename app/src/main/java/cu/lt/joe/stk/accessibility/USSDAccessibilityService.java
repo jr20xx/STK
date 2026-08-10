b0cc842e3a211539000ffb5ca9d083a4f8b0fd12
jr20xx <pprichard0322@gmail.com>
2026-08-10 09:42:30 -0400

    Updated the code to remove consulted data when it's no longer present in the USSD message response

--- app/src/main/java/cu/lt/joe/stk/accessibility/USSDAccessibilityService.java
+++ app/src/main/java/cu/lt/joe/stk/accessibility/USSDAccessibilityService.java
@@ -13,6 +13,10 @@ import cu.lt.joe.stk.utils.Utils;
 @SuppressLint("AccessibilityPolicy")
 public class USSDAccessibilityService extends AccessibilityService
 {
+    private final String TERMINATED_DATA_PACKET_CONTENT_MESSAGE = "Ud debe adquirir una oferta",
+            TERMINATED_SMS_PACKET_CONTENT_MESSAGE = "Usted debe adquirir un plan de SMS",
+            TERMINATED_VOICE_PACKET_CONTENT_MESSAGE = "Usted debe adquirir un plan de minutos";
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event)
     {
@@ -22,34 +26,46 @@ public class USSDAccessibilityService extends AccessibilityService
         if (event.getClassName() != null && AlertDialog.class.getName().contentEquals(event.getClassName()))
         {
             String responseMessage = Utils.extractTextFromAccessibilityEvent(event).toUpperCase();
-            String wordsFromMessage[] = responseMessage.toUpperCase().split(" ");
-            for (int i = 0; i < wordsFromMessage.length; i++)
+            if (responseMessage.contains(TERMINATED_DATA_PACKET_CONTENT_MESSAGE.toUpperCase()))
+                spEditor.putString(Constants.LAST_KNOWN_INTERNET_DATA, null).apply();
+            else if (responseMessage.contains(TERMINATED_VOICE_PACKET_CONTENT_MESSAGE.toUpperCase()))
+                spEditor.putString(Constants.LAST_KNOWN_MINUTES_COUNT, null).apply();
+            else if (responseMessage.contains(TERMINATED_SMS_PACKET_CONTENT_MESSAGE.toUpperCase()))
+                spEditor.putString(Constants.LAST_KNOWN_MESSAGES_COUNT, null).apply();
+            else
             {
-                String currentWord = wordsFromMessage[i];
-                switch (currentWord)
+                String wordsFromMessage[] = responseMessage.toUpperCase().split(" ");
+                for (int i = 0; i < wordsFromMessage.length; i++)
                 {
-                    case "SALDO:":
-                        spEditor.putString(Constants.LAST_KNOWN_BALANCE, wordsFromMessage[i + 1] + " " + wordsFromMessage[i + 2].replace(".", "")).apply();
-                        i += 2;
-                        break;
-                    case "DATOS:":
-                        spEditor.putString(Constants.LAST_KNOWN_INTERNET_DATA, wordsFromMessage[i + 1] + " " + wordsFromMessage[i + 2].replace(".", "")).apply();
-                        i += 2;
-                        break;
-                    case "VOZ:":
-                        spEditor.putString(Constants.LAST_KNOWN_MINUTES_COUNT, wordsFromMessage[i + 1].replace(".", "")).apply();
-                        i++;
-                        break;
-                    case "SMS:":
-                        spEditor.putString(Constants.LAST_KNOWN_MESSAGES_COUNT, wordsFromMessage[i + 1].replace(".", "")).apply();
-                        i++;
-                        break;
-                    case "SMS":
-                        spEditor.putString(Constants.LAST_KNOWN_MESSAGES_COUNT, wordsFromMessage[i - 1]).apply();
-                        break;
-                    case "MIN":
-                        spEditor.putString(Constants.LAST_KNOWN_MINUTES_COUNT, wordsFromMessage[i - 1]).apply();
-                        break;
+                    String currentWord = wordsFromMessage[i];
+                    switch (currentWord)
+                    {
+                        case "SALDO:":
+                            spEditor.putString(Constants.LAST_KNOWN_BALANCE, wordsFromMessage[i + 1] + " " + wordsFromMessage[i + 2].replace(".", "")).apply();
+                            spEditor.putString(Constants.LAST_KNOWN_INTERNET_DATA, null).apply();
+                            spEditor.putString(Constants.LAST_KNOWN_MINUTES_COUNT, null).apply();
+                            spEditor.putString(Constants.LAST_KNOWN_MESSAGES_COUNT, null).apply();
+                            i += 2;
+                            break;
+                        case "DATOS:":
+                            spEditor.putString(Constants.LAST_KNOWN_INTERNET_DATA, wordsFromMessage[i + 1] + " " + wordsFromMessage[i + 2].replace(".", "")).apply();
+                            i += 2;
+                            break;
+                        case "VOZ:":
+                            spEditor.putString(Constants.LAST_KNOWN_MINUTES_COUNT, wordsFromMessage[i + 1].replace(".", "")).apply();
+                            i++;
+                            break;
+                        case "SMS:":
+                            spEditor.putString(Constants.LAST_KNOWN_MESSAGES_COUNT, wordsFromMessage[i + 1].replace(".", "")).apply();
+                            i++;
+                            break;
+                        case "SMS":
+                            spEditor.putString(Constants.LAST_KNOWN_MESSAGES_COUNT, wordsFromMessage[i - 1]).apply();
+                            break;
+                        case "MIN":
+                            spEditor.putString(Constants.LAST_KNOWN_MINUTES_COUNT, wordsFromMessage[i - 1]).apply();
+                            break;
+                    }
                 }
             }
         }
