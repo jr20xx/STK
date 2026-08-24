785f9ef1a12f1948c9b62e8d4a16f9352c69063a
jr20xx <pprichard0322@gmail.com>
2026-08-24 19:10:59 -0400

    Added an icon to represent the crash logs

--- app/src/main/java/cu/lt/joe/stk/activities/MainActivity.java
+++ app/src/main/java/cu/lt/joe/stk/activities/MainActivity.java
@@ -85,7 +85,7 @@ public class MainActivity extends BaseActivity
             BottomSheetDialog additionalOptionsMenu = new BottomSheetDialog(this);
             AdditionalOptionsMenuLayoutBinding menuLayoutBinding = AdditionalOptionsMenuLayoutBinding.inflate(getLayoutInflater());
             ArrayList<AdditionalOptionsMenuItem> additionalOptionsMenuItems = new ArrayList<>();
-            additionalOptionsMenuItems.add(new AdditionalOptionsMenuItem(R.drawable.ic_bonus, "Registro de  fallos", new Intent(this, CrashLogsViewerActivity.class)));
+            additionalOptionsMenuItems.add(new AdditionalOptionsMenuItem(R.drawable.ic_crash_log, "Registro de  fallos", new Intent(this, CrashLogsViewerActivity.class)));
             menuLayoutBinding.additionalOptionsMenuRecycler.setAdapter(new AdditionalOptionsMenuItemAdapter(this, additionalOptionsMenuItems));
             additionalOptionsMenu.setContentView(menuLayoutBinding.getRoot());
             additionalOptionsMenu.show();
