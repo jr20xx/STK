91b367a53f20d0c9cb082444291cdb7432f44c37
jr20xx <pprichard0322@gmail.com>
2026-08-26 11:02:50 -0400

    Updated the code to work with the new distribiution of items in the navigation bar

--- app/src/main/java/cu/lt/joe/stk/activities/MainActivity.java
+++ app/src/main/java/cu/lt/joe/stk/activities/MainActivity.java
@@ -1,22 +1,16 @@
 package cu.lt.joe.stk.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager2.widget.ViewPager2;
-import com.google.android.material.bottomsheet.BottomSheetDialog;
-import java.util.ArrayList;
 import cu.lt.joe.stk.R;
-import cu.lt.joe.stk.adapters.AdditionalOptionsMenuItemAdapter;
 import cu.lt.joe.stk.adapters.MainActivityPagerAdapter;
-import cu.lt.joe.stk.databinding.AdditionalOptionsMenuLayoutBinding;
 import cu.lt.joe.stk.databinding.MainLayoutBinding;
 import cu.lt.joe.stk.fragments.MainFragment;
 import cu.lt.joe.stk.fragments.SettingsFragment;
 import cu.lt.joe.stk.fragments.ShoppingFragment;
-import cu.lt.joe.stk.objects.AdditionalOptionsMenuItem;
 
 public class MainActivity extends BaseActivity
 {
@@ -46,10 +40,10 @@ public class MainActivity extends BaseActivity
                 binding.mainActivityTitleTv.setText(R.string.shop_fragment_title);
                 return true;
             }
-            else if (item.getTitle().toString().equals(getString(R.string.settings_fragment_menu_title)))
+            else if (item.getTitle().toString().equals(getString(R.string.more_options_fragment_menu_title)))
             {
                 binding.mainActivityPager.setCurrentItem(2);
-                binding.mainActivityTitleTv.setText(R.string.settings_fragment_menu_title);
+                binding.mainActivityTitleTv.setText(R.string.more_options_fragment_menu_title);
                 return true;
             }
             return false;
@@ -68,7 +62,7 @@ public class MainActivity extends BaseActivity
                         binding.bottomNavigationView.setSelectedItemId(R.id.shopping_fragment_im);
                         break;
                     case 2:
-                        binding.bottomNavigationView.setSelectedItemId(R.id.settings_fragment_im);
+                        binding.bottomNavigationView.setSelectedItemId(R.id.more_options_im);
                         break;
                     default:
                         super.onPageSelected(position);
@@ -77,16 +71,5 @@ public class MainActivity extends BaseActivity
         });
         setSupportActionBar(binding.mainActivityToolbar);
         binding.mainActivityPager.setOffscreenPageLimit(2);
-        binding.additionalOptionsMenuOpenerButton.setOnClickListener(v ->
-        {
-            //TODO: Write the code to open the menu with additional stuffs
-            BottomSheetDialog additionalOptionsMenu = new BottomSheetDialog(this);
-            AdditionalOptionsMenuLayoutBinding menuLayoutBinding = AdditionalOptionsMenuLayoutBinding.inflate(getLayoutInflater());
-            ArrayList<AdditionalOptionsMenuItem> additionalOptionsMenuItems = new ArrayList<>();
-            additionalOptionsMenuItems.add(new AdditionalOptionsMenuItem(R.drawable.ic_crash_log, "Registro de  fallos", new Intent(this, CrashLogsViewerActivity.class)));
-            menuLayoutBinding.additionalOptionsMenuRecycler.setAdapter(new AdditionalOptionsMenuItemAdapter(this, additionalOptionsMenuItems));
-            additionalOptionsMenu.setContentView(menuLayoutBinding.getRoot());
-            additionalOptionsMenu.show();
-        });
     }
 }
