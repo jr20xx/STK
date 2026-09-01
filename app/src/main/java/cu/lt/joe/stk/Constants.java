50940513e2ab5102737f86878fea375ddec19f50
jr20xx <pprichard0322@gmail.com>
2026-09-01 01:03:07 -0400

    Added a dialog to select the date format to use in the list of crash logs

--- app/src/main/java/cu/lt/joe/stk/Constants.java
+++ app/src/main/java/cu/lt/joe/stk/Constants.java
@@ -12,7 +12,8 @@ public class Constants
             LAST_KNOWN_BONUSES = "last_known_bonuses",
             LAST_KNOWN_RECHARGE_TIME = "last_known_recharge_time",
             REVERTED_CALL_PREFIX_INDEX = "reverted_call_prefix_index",
-            SAVED_LOCALE_TAG = "saved_locale_tag";
+            SAVED_LOCALE_TAG = "saved_locale_tag",
+            SAVED_DATE_FORMAT = "saved_date_format";
     public final static String REVERTED_CALL_PREFIXES_ARRAY[] = new String[]{"*99", "*+99", "+5399"},
             LANGUAGE_TAGS[] = new String[]{null, "es", "en"};
 }
---
+++ app/src/main/java/cu/lt/joe/stk/fragments/dialog_fragments/DateFormatPickerDialogFragment.java
@@ -0,0 +1,44 @@
+package cu.lt.joe.stk.fragments.dialog_fragments;
+
+import android.app.Dialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.preference.PreferenceManager;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import java.util.Arrays;
+import java.util.List;
+import cu.lt.joe.stk.Constants;
+import cu.lt.joe.stk.R;
+
+public class DateFormatPickerDialogFragment extends DialogFragment
+{
+    private SharedPreferences sharp;
+    private SharedPreferences.Editor preferencesEditor;
+    private int currentlySelectedItemIndex;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState)
+    {
+        sharp = PreferenceManager.getDefaultSharedPreferences(requireContext());
+        preferencesEditor = sharp.edit();
+
+        List<String> dateFormatsLabels = Arrays.asList(getResources().getStringArray(R.array.date_formats_labels));
+        List<String> dateFormats = Arrays.asList(getResources().getStringArray(R.array.date_formats));
+
+        currentlySelectedItemIndex = dateFormats.indexOf(sharp.getString(Constants.SAVED_DATE_FORMAT, dateFormats.get(0)));
+
+        return new MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("Date format")
+                .setSingleChoiceItems(dateFormatsLabels.toArray(new String[0]), currentlySelectedItemIndex, (dialog, which) -> currentlySelectedItemIndex = which)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                    preferencesEditor.putString(Constants.SAVED_DATE_FORMAT, dateFormats.get(currentlySelectedItemIndex)).apply();
+                    dismissAllowingStateLoss();
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .create();
+    }
+}
